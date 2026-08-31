71326654c7616314c82af310e3d18f94719c312a
Victor Duro <victor.duro@gmail.com>
2026-08-30 22:33:20 -0300

    feat: pratica declaracao de variaveis e seus tipos

---
+++ js/exercicio02.js
@@ -0,0 +1,23 @@
+const nome = "Victor Duro";
+let idade = 41;
+const cidade = "Vargem Grande Paulista";
+const matriculado = true;
+const nota = 9.5;
+
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Cidade:", cidade);
+console.log("Matriculado:", matriculado);
+console.log("Nota:", nota);
+
+console.log("Tipo do nome:", typeof nome);
+console.log("Tipo da idade:", typeof idade);
+console.log("Tipo da cidade:", typeof cidade);
+console.log("Tipo de matriculado:", typeof matriculado);
+console.log("Tipo da nota:", typeof nota);
+
+/*
+A variável idade foi declarada com let porque seu valor pode ser alterado durante a execução do programa.
+
+As outras variáveis foram declaradas com const porque seus valores não precisam ser alterados.
+*/
