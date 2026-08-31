1b7f3c0bfd7605b14db0efaf384631e8044a3029
Victor Duro <victor.duro@gmail.com>
2026-08-31 10:41:59 -0300

    feat: adiciona verificacao simples de login

---
+++ js/exercicio07.js
@@ -0,0 +1,13 @@
+const usuario = prompt("Digite o usuário:");
+const senha = prompt("Digite a senha:");
+
+if (usuario === "admin" && senha === "1234") {
+    console.log("Acesso permitido");
+} else {
+    console.log("Acesso negado");
+}
+
+
+//O sinal = é usado para atribuir um valor a uma variável.
+
+//O sinal === é usado para comparar valores e também verificar se os tipos são iguais.
