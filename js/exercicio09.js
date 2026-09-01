84806fcd0c291e840b024cb718b8796060e9c9a9
Victor Duro <victor.duro@gmail.com>
2026-09-01 10:55:57 -0300

    feat: processa entradas numericas com while

---
+++ js/exercicio09.js
@@ -0,0 +1,32 @@
+let quantidade = 0;
+let soma = 0;
+let maior = -Infinity;
+let menor = Infinity;
+
+let valor = Number(prompt("Digite um número. Digite 0 para encerrar."));
+
+while (valor !== 0) {
+    quantidade++;
+    soma += valor;
+
+    if (valor > maior) {
+        maior = valor;
+    }
+    if (valor < menor) {
+        menor = valor;
+    }
+
+    valor = Number(prompt("Digite outro número. Digite 0 para encerrar."));
+}
+
+if (quantidade === 0) {
+    console.log("Nenhum número foi informado.");
+} else {
+    let media = soma / quantidade;
+
+    console.log("Quantidade de números:", quantidade);
+    console.log("Soma:", soma);
+    console.log("Média:", media);
+    console.log("Maior número:", maior);
+    console.log("Menor número:", menor);
+}
